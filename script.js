63ebb716e124495e011d046917a83b23cd386984
DuyLuu90 <duyluu90@hotmail.com>
2020-02-09 18:50:15 -0700

    update files

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
-'use strict'// undeclared variables not allowed
+'use strict';// undeclared variables not allowed
+
 function generateQuestion (item) {
   console.log('\nGenerating question')
+  ///*
+  return `
+  <div class='js-questions'>
+    <legend>${item.question}</legend>
+    <img src='${item.image}' class='question-picture' alt='Question-Picture'>
+  </div>
+  <div class='js-options'>
+    <input type='radio' id='${item.id}-0' value='0'>
+    <label for='${item.id}-0'>${item.option[0]}</label>
+    <br>
+    <input type='radio' id='${item.id}-1' value='1'>
+    <label for='${item.id}-1'>${item.option[1]}</label>
+    <br>
+    <input type='radio' id='${item.id}-2' value='2'>
+    <label for='${item.id}-2'>${item.option[2]}</label>
+    <br>
+    <input type='radio' id='${item.id}-3' value='3'>
+    <label for='${item.id}-3'>${item.option[3]}</label>
+    <br>  
+  </div>
+  <div class='js-submit'>
+    <button type='submit'class='submit'> SUBMIT </button>
+  </div> `
+  //*/
 }
 function generateQuizBank(){
   console.log('\nGenerating Quiz-Bank')
 };
-function renderQuizBank(){
-  console.log('\nRendering Quiz-Bank to the DOM')
+function renderAQuestion(){
+  console.log('\nRendering a question to the DOM')
+  const html=generateQuestion(quizBank[user.currentQuestion]);
+  $('.questions-screen').html(html);
+
 };
 function handleStartClicked(){
   console.log('\nHandling Start-cliked')
   //this function display the question 
+  $('.start').on('click', event=>renderAQuestion());
 };
 function handleAnswerClicked(){
   console.log('\nHandling Answer-cliked')
@@ -20,12 +49,10 @@ function handleAnswerSubmitted(){
   //this function (1)'receive' the Answer 'cliked' by the user, (2)compare it with the correct Answer, (3) alert the user if his/her ans is correct or not, (4) record the score, then (5) move on to the next question in the quiz bank 
 };
 
-
-
 function runQuizApp() {
   generateQuestion()
   generateQuizBank ()
-  renderQuizBank();
+  renderAQuestion();
   handleStartClicked();
   handleAnswerClicked();
   handleAnswerSubmitted();
--- store.js
+++ store.js
@@ -1,17 +1,15 @@
-const quizBank = [
-    {
-      id: cuid(),
-      //generate ID
-      question: '',
-      image: '',
-      option: ['','','',''],
-      answer: ''
-    }
-  ]
-  
-  const store={
-    //current question 
-    //user's anwer choice(s)
-    //current view
-    //score
+const quizBank=[
+  { id: cuid(),
+    question: 'What year is it?',
+    image: '',
+    option: ['2000','2010','2020','2030'],
+    answer: '2020'
+  }
+]
+ 
+const user= {
+    currentQuestion: 0, 
+    userAnswer: '',
+    currentView: '',
+    score: 0
   }

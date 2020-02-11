7d54c22234785539bfd72034636c5cbb37c79ff8
DuyLuu90 <duyluu90@hotmail.com>
2020-02-10 23:51:07 -0700

    update all files

--- script.js
+++ script.js
@@ -1,60 +1,147 @@
 'use strict';// undeclared variables not allowed
 
-function generateQuestion (item) {
-  console.log('\nGenerating question')
-  ///*
-  return `
-  <div class='js-questions'>
-    <legend>${item.question}</legend>
-    <img src='${item.image}' class='question-picture' alt='Question-Picture'>
-  </div>
-  <div class='js-options'>
-    <input type='radio' id='${item.id}-0' value='0'>
-    <label for='${item.id}-0'>${item.option[0]}</label>
-    <br>
-    <input type='radio' id='${item.id}-1' value='1'>
-    <label for='${item.id}-1'>${item.option[1]}</label>
-    <br>
-    <input type='radio' id='${item.id}-2' value='2'>
-    <label for='${item.id}-2'>${item.option[2]}</label>
-    <br>
-    <input type='radio' id='${item.id}-3' value='3'>
-    <label for='${item.id}-3'>${item.option[3]}</label>
-    <br>  
-  </div>
-  <div class='js-submit'>
-    <button type='submit'class='submit'> SUBMIT </button>
-  </div> `
-  //*/
-}
-function generateQuizBank(){
-  console.log('\nGenerating Quiz-Bank')
+function handleStartClicked(){
+  console.log('\nHandling Start-cliked');
+  //this function display the question 
+  $('main').on('click','#start',event =>renderAQuestion());
 };
+
+function updateQuestionAndScore() {
+  const html= `
+  <span class='currentQuestion'> Question: ${user.currentQuestion+1}/${quizBank.length} </span> 
+  <span class='currentScore'> Score: ${user.score}/${quizBank.length} </span> `;
+  $('.questions-scores').html(html);
+}
+
+function generateQuestion(item){
+  console.log('\nGenerating question');
+  return `
+    <form id='js-questions' method='post'>
+      <fieldset >
+        <div class='questions'>
+          <legend>${item.question}</legend>
+          <img src='${item.image}' class='question-picture' alt='Question-Picture'>
+        </div>
+        <div class='options'>
+          
+        </div>
+        <div class='submit'>
+          <button type='submit' id='submit'> SUBMIT </button>
+        </div> 
+      </Fieldset>
+    </form>`;
+}
+
+function generateOptions(array,item) {
+  let optionHtml='';
+  for (let i=0; i<array.length;i++) {
+      optionHtml += `
+      <input type='radio' name='option' id='${item.id}-${i}' value='${array[i]}'>
+      <label for='${item.id}-${i}'>${array[i]}</label>
+      <br>`
+  };
+  return optionHtml;
+}
+
 function renderAQuestion(){
   console.log('\nRendering a question to the DOM')
-  const html=generateQuestion(quizBank[user.currentQuestion]);
-  $('.questions-screen').html(html);
+  updateQuestionAndScore();
+  let question= quizBank[user.currentQuestion];
+  let options=question.option; 
+  const html= generateQuestion(question);
+  $('.main').html(html);//replace the html of main class
+  const optionHtml= generateOptions(options,question);
+  $('.options').html(optionHtml);
+  //user.currentQuestion ++;
+};
+function createAlertBox (message) {
+  return `
+    <fieldset class='alert'>
+      <div class='message'>
+        <p> ${message}</p>
+      </div>
+      <div class='submit'>
+        <button type='select'ID='Next'>NEXT</button>
+      </div>
+    </fieldset>
+  `
+}
+function handleNextClicked() {
+  $('main').on('click','#Next', function(event) {
+    $('#js-questions').show();
+    $('.alert').empty();
+    user.currentQuestion === quizBank.length?
+    displayFinalScreen():renderAQuestion();
+  })
+}
 
-};
-function handleStartClicked(){
-  console.log('\nHandling Start-cliked')
-  //this function display the question 
-  $('.start').on('click', event=>renderAQuestion());
-};
-function handleAnswerClicked(){
-  console.log('\nHandling Answer-cliked')
-};
 function handleAnswerSubmitted(){
-  console.log('\nHandling Answer-submitted')
+  console.log('\nHandling Answer-submitted');
+  $('main').on('submit','#js-questions',event=>{
+    event.preventDefault();
+    $('#js-questions').hide();
+    let result=quizBank[user.currentQuestion].answer;
+    //(1) receive the answer 'selected'
+    //Q: we can't use let/const to assign the property value to a variable
+    user.answer= $("input[name='option']:checked").val();
+    if (!user.answer) {
+      $('main').append(`${createAlertBox('Please choose an option')}`);
+    }
+    else if (user.answer===result) {
+      $('main').append(`${createAlertBox('Your answer is correct')}`);
+      user.score ++;
+      user.currentQuestion ++;
+    }
+    else {
+      $('main').append(`${createAlertBox(`That's wrong.The correct answer is ${result}.`)}`);
+      user.currentQuestion ++;
+    }
+    /*
+    user.currentQuestion === quizBank.length?
+    displayFinalScreen(): renderAQuestion();
+    */
+  });
+
   //this function (1)'receive' the Answer 'cliked' by the user, (2)compare it with the correct Answer, (3) alert the user if his/her ans is correct or not, (4) record the score, then (5) move on to the next question in the quiz bank 
 };
 
-function runQuizApp() {
-  generateQuestion()
-  generateQuizBank ()
-  renderAQuestion();
-  handleStartClicked();
-  handleAnswerClicked();
-  handleAnswerSubmitted();
+function displayFinalScreen() {
+  console.log('\nDisplaying Final Screen');
+  let finalHtml= `
+    <fieldset class='final-screen'>
+      <div class='message'>
+        <p>THANK YOU</p>
+        <p> You have completed your quiz. Your score is <span> ${user.score}/${quizBank.length} </span>. What would you like to do next?</p>
+      </div>
+      <div class='submit'>
+        <button type='select' ID='exit'>EXIT</button>
+        <button type='select' ID='startOver'>START OVER</button>
+      </div>
+    </fieldset>` ;
+  user.currentQuestion=0;
+  user.score=0;
+  $('.questions-scores').empty();
+  $('main').html(finalHtml);
 }
-$(runQuizApp); //when the page loads, call 'runQuizApp'
+function restartQuiz() {
+  $('main').on('click','#startOver', event=> renderAQuestion());
+}
+function exitQuiz() {
+  $('main').on('click','#exit', event=>location.reload());
+}
+/*
+function generateQuizBank(){
+  console.log('\nGenerating Quiz-Bank');
+*/
+function runQuizApp() {
+  handleStartClicked();
+  handleNextClicked();
+  //generateQuestion();// this function needs not to be called when the page loads.
+  //generateQuizBank ();
+  //renderAQuestion();  
+  handleAnswerSubmitted();
+  //displayFinalScreen();
+  restartQuiz();
+  exitQuiz();
+} 
+$(runQuizApp);
--- store.js
+++ store.js
@@ -1,15 +1,64 @@
 const quizBank=[
   { id: cuid(),
-    question: 'What year is it?',
-    image: '',
-    option: ['2000','2010','2020','2030'],
-    answer: '2020'
+    question: 'What is the offical name of Taiwan?',
+    image: 'https://tnimage.s3.hicloud.net.tw/photos/shares/5afa69ff9b7b6.png',
+    option: [
+      'People Republic of China',
+      'Kingdom of Great China',
+      'Republic of China',
+      'United of China'
+    ],
+    answer: 'Republic of China'
+  },
+  { id: cuid(),
+    question: 'Summer 2020 Olympics will be held in which city?',
+    image: 'https://jw-webmagazine.com/wp-content/uploads/2019/06/jw-5d15da96c49484.64193252.jpeg',
+    option: [
+      'Tokyo',
+      'Osaka',
+      'Nagoya',
+      'Sapporo'
+    ],
+    answer: 'Tokyo'
+  },
+  { id: cuid(),
+    question: 'What was the capital of Vietnam from 1802 to 1945?',
+    image: 'https://thuathienhue.gov.vn/Portals/0/Medias/Nam2018/T3/Thai%20Hoa.jpg',
+    option: [
+      'Hanoi',
+      'Hue',
+      'Saigon',
+      'Hoian'
+    ],
+    answer: 'Hue'
+  },
+  { id: cuid(),
+    question: 'The Forbidden City is located in the heart of which city?',
+    image: 'https://funlifecrisis.com/wp-content/uploads/2019/10/guide-to-visiting-the-forbidden-city-beijing-china.jpg',
+    option: [
+      'Beijing',
+      'Shanghai',
+      'Nanjing',
+      'Changan'
+    ],
+    answer: 'Beijing'
+  },
+  { id: cuid(),
+    question: 'What is the capital of South Korea?',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Samsung_headquarters.jpg/1024px-Samsung_headquarters.jpg',
+    option: [
+      'Seoul',
+      'Busan',
+      'Incheon',
+      'Pyongyang'
+    ],
+    answer: 'Seoul'
   }
 ]
  
 const user= {
     currentQuestion: 0, 
-    userAnswer: '',
+    answer: '',
     currentView: '',
     score: 0
   }
